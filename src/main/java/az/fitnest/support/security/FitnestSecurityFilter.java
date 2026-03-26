499d537ca06c8423ca39d2f2c267b83ce18c1b8a
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-03-26 14:38:35 +0400

    chore: add @Component annotation to FitnestSecurityFilter

--- src/main/java/az/fitnest/support/security/FitnestSecurityFilter.java
+++ src/main/java/az/fitnest/support/security/FitnestSecurityFilter.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
+@Component
 public class FitnestSecurityFilter extends OncePerRequestFilter {
 
     @Override
